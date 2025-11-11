f94da958ad69799f7c00b4911826cf8a2d61ddf3
Sumi Akter <sumiwdcse@gmail.com>
2025-11-11 23:20:44 +0600

    Initial commit - PlateShare server setup and get data from db

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port =process.env.PORT || 3000 ;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// MongoDB Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ztnzz8f.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Use existing DB and collection
+    const db = client.db("food-sharing-db");
+    const foodsCollection = db.collection("food");
+
+    console.log(" MongoDB connected successfully!");
+
+    // Root route
+    app.get("/", (req, res) => {
+      res.send(" PlateShare server is running!");
+    });
+
+    // GET all foods from collection
+    app.get("/foods", async (req, res) => {
+      try {
+        const foods = await foodsCollection.find().toArray();
+        res.send(foods);
+      } catch (error) {
+        console.error(" Error fetching foods:", error);
+        res.status(500).send({ message: "Failed to fetch foods" });
+      }
+    });
+
+    // Find by food_status
+    app.get("/foods/status/:status", async (req, res) => {
+      try {
+        const status = req.params.status;
+        const result = await foodsCollection.find({ food_status: status }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(" Error fetching by status:", error);
+        res.status(500).send({ message: "Failed to fetch filtered foods" });
+      }
+    });
+
+ 
+
+    await db.command({ ping: 1 });
+    console.log("Pinged your deployment successfully.");
+  } catch (error) {
+    console.error(" Error connecting to MongoDB:", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(` Server running on port ${port}`);
+});
